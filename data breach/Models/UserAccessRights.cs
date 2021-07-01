eba2f72442c7cf891568d995840e86bf077d1e5c
Aditya Singh1 <Aditya_Singh1@gep.com>
2021-07-01 23:27:59 +0530

    Edited based on Database Requirements

--- data breach/Models/UserAccessRights.cs
+++ data breach/Models/UserAccessRights.cs
@@ -1,16 +1,16 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace data_breach.Models
 {
-    public class User
+    public class UserAccessRights
     {
         [BsonId]
         public Guid Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
         public string UserRole { get; set; }
+        public string CollectionName { get; set; }
+        public string AccessString { get; set; }
     }
 }
